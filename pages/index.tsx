1504f585b9b73096a8587f892f6d1f7fe471d241
Matheus Pavanelli <mspavanelli@gmail.com>
2021-01-29 23:43:26 -0300

    :sparkles: feat: add fetching action

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 const Home = () => {
+  const fetchInvoices = async () => {
+    try {
+      const response = await fetch('/api/invoices')
+
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+
+      const parsedData = await response.json()
+
+      console.log(parsedData)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <main className="layout">
-      <button className="btn btn-white">Fetch Invoices</button>
+      <button className="btn btn-white" onClick={fetchInvoices}>
+        Fetch Invoices
+      </button>
     </main>
   )
 }
